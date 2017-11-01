786d1590a4c89e79808936c2973cfea81074b134
alpaca-tc <alpaca-tc@alpaca.tc>
2017-11-01 15:14:30 +0900

    Remove useless lines

--- lib/switch_db/cli.rb
+++ lib/switch_db/cli.rb
@@ -9,29 +9,7 @@ module SwitchDb
 
         command_class.new(reference_set: reference_set).run(arguments)
 
-        # case @command.to_sym
-        # when :list
-        # when :rm
-        #   reference = find_by_name(@args[:name])
-        #   Rm.new(reference).rm!
-        #   reference_set.remove_reference(reference)
-        #   reference_set.write_reference_set!
-        #
-        #   puts "Remove #{reference.name}"
-        # when :store
-        #   reference = Reference.new(name: @args[:name], database_names: @args[:database_names], metadata: { created_at: Time.now })
-        #   reference_set.add_reference(reference)
-        #   Store.new(reference).store!
-        #
-        #   reference_set.write_reference_set!
-        #   puts "Store #{@args[:database_names]} to #{@args[:name]}"
-        # when :restore
-        #   reference = find_by_name(@args[:name])
-        #   Restore.new(reference).restore!
-        #   puts "Restore #{reference.database_names} to #{reference.name}"
-        # else
-        #   raise "Unknown command given #{@command} with #{@args}"
-        # end
+        reference_set.write_reference_set
       end
 
       private
