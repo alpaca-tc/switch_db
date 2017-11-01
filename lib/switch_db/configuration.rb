c7bec993335f3f0cb02f49a6be947b2816066af5
alpaca-tc <alpaca-tc@alpaca.tc>
2017-11-01 15:22:55 +0900

    Add user_name and password

--- lib/switch_db/configuration.rb
+++ lib/switch_db/configuration.rb
@@ -4,10 +4,14 @@ module SwitchDb
   class Configuration
     attr_accessor :reference_set_filename
     attr_accessor :cache_dir
+    attr_accessor :user_name
+    attr_accessor :password
 
     def initialize
       @reference_set_filename = '.switch_db.yml'
       @cache_dir = Pathname.new(File.expand_path('~/.cache/switch_db'))
+      @user_name = 'root'
+      @password = ''
     end
   end
 end
