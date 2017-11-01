9d8809253d10824490a00ee8da0956fdbfeed4f8
alpaca-tc <alpaca-tc@alpaca.tc>
2017-11-01 16:03:52 +0900

    Use configured user_name

--- lib/switch_db/database/mysql.rb
+++ lib/switch_db/database/mysql.rb
@@ -4,19 +4,19 @@ module SwitchDb
       extend Abstract
 
       def self.drop_database(database_name)
-        `mysql -u root -p --execute="DROP DATABASE #{database_name}"`
+        `mysql -u #{SwitchDb.configuration.user_name} -p --execute="DROP DATABASE #{database_name}"`
       end
 
       def self.create_database(database_name)
-        `mysql -u root -p --execute="CREATE DATABASE #{database_name}"`
+        `mysql -u #{SwitchDb.configuration.user_name} -p --execute="CREATE DATABASE #{database_name}"`
       end
 
       def self.import_database(database_name, database_path)
-        `mysql -u root -p #{database_name} < #{database_path}`
+        `mysql -u #{SwitchDb.configuration.user_name} -p #{database_name} < #{database_path}`
       end
 
       def self.dump_database(database_name, output_path)
-        `mysqldump -u root -p #{database_name} > #{output_path}`
+        `mysqldump -u #{SwitchDb.configuration.user_name} -p #{database_name} > #{output_path}`
       end
     end
   end
