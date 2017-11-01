b2a8efa51bb6443ac304532e7b2330462cebb139
alpaca-tc <alpaca-tc@alpaca.tc>
2017-11-01 13:21:23 +0900

    Add spec for SwitchDb

--- lib/switch_db.rb
+++ lib/switch_db.rb
@@ -1,23 +1,24 @@
-require 'pry'
-require 'switch_db/version'
-require 'switch_db/store'
-require 'switch_db/restore'
-require 'switch_db/configuration'
-require 'switch_db/utils'
 require 'switch_db/cli'
+require 'switch_db/configuration'
+require 'switch_db/dialog'
+require 'switch_db/option_parser'
 require 'switch_db/reference'
 require 'switch_db/reference_set'
-require 'switch_db/option_parser'
-require 'switch_db/rm'
-require 'switch_db/dialog'
+require 'switch_db/utils'
+require 'switch_db/version'
+require 'switch_db/command/store'
+require 'switch_db/command/restore'
+require 'switch_db/command/rm'
 
 module SwitchDb
   def self.configuration
-    @configuration = Configuration.new
+    @configuration ||= Configuration.new
   end
 
   def self.run(argv)
     args = OptionParser.new(argv).parse!
-    Cli.new(args[:command], args).run!
+    command = args.delete(:command)
+
+    Cli.new(command, args).run!
   end
 end
--- spec/switch_db_spec.rb
+++ spec/switch_db_spec.rb
@@ -1,11 +1,21 @@
 require "spec_helper"
 
 RSpec.describe SwitchDb do
-  it "has a version number" do
-    expect(SwitchDb::VERSION).not_to be nil
-  end
+  describe 'ClassMethods' do
+    describe '.configuration' do
+      subject { described_class.configuration }
+      it { is_expected.to be_a(described_class::Configuration) }
+    end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+    describe '.run' do
+      subject do
+        described_class.run(['list'])
+      end
+
+      it 'calls command' do
+        expect_any_instance_of(described_class::Cli).to receive(:initialize).with('list', {})
+        subject
+      end
+    end
   end
 end
