d0516a18f46a4dcedc709e0a16b1163f46a2147d
alpaca-tc <alpaca-tc@alpaca.tc>
2017-11-01 16:11:25 +0900

    Replace snapshot -> dumpfile

--- lib/switch_db/option_parser.rb
+++ lib/switch_db/option_parser.rb
@@ -47,7 +47,7 @@ module SwitchDb
       <<~BANNER
         switch_db v#{SwitchDb::VERSION}
 
-        switch_db is a snapshot database manager. This is a basic help message.
+        switch_db is a database dupfile manager. This is a basic help message.
           Usage:
             switch_db list
             switch_db config [key:value...]
