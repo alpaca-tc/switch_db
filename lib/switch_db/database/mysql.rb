63d9a479a3b19b56724680f872d797444af40282
alpaca-tc <alpaca-tc@alpaca.tc>
2017-11-02 15:33:58 +0900

    Ignore password

--- lib/switch_db/database/mysql.rb
+++ lib/switch_db/database/mysql.rb
@@ -3,20 +3,31 @@ module SwitchDb
     module Mysql
       extend Abstract
 
-      def self.drop_database(database_name)
-        `mysql -u #{SwitchDb.configuration.user_name} -p --execute="DROP DATABASE #{database_name}"`
-      end
+      class << self
+        def drop_database(database_name)
+          `mysql #{user_and_password} --execute="DROP DATABASE #{database_name}"`
+        end
 
-      def self.create_database(database_name)
-        `mysql -u #{SwitchDb.configuration.user_name} -p --execute="CREATE DATABASE #{database_name}"`
-      end
+        def create_database(database_name)
+          `mysql #{user_and_password} --execute="CREATE DATABASE #{database_name}"`
+        end
 
-      def self.import_database(database_name, database_path)
-        `mysql -u #{SwitchDb.configuration.user_name} -p #{database_name} < #{database_path}`
-      end
+        def import_database(database_name, database_path)
+          `mysql #{user_and_password} #{database_name} < #{database_path}`
+        end
 
-      def self.dump_database(database_name, output_path)
-        `mysqldump -u #{SwitchDb.configuration.user_name} -p #{database_name} > #{output_path}`
+        def dump_database(database_name, output_path)
+          `mysqldump #{user_and_password} #{database_name} > #{output_path}`
+        end
+
+        private
+
+        def user_and_password
+          arguments = ["-u #{SwitchDb.configuration.user_name}"]
+          arguments.push("-p#{SwitchDb.configuration.password}") if SwitchDb.configuration.password
+
+          arguments.join(' ')
+        end
       end
     end
   end
